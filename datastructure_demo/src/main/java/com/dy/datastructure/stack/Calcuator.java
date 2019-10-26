c655079dc61e32bbc78bb36209d3ded3a6335c13
dy <1439824709@qq.com>
2019-10-26 19:57:28 +0800

    1.数组模拟栈 2.模拟计算器中缀计算 3.链表模拟战

---
+++ datastructure_demo/src/main/java/com/dy/datastructure/stack/Calcuator.java
@@ -0,0 +1,12 @@
+package com.dy.datastructure.stack;
+
+import java.util.Stack;
+
+public class Calcuator {
+
+
+    ArrayStack operator=new ArrayStack();
+
+
+
+}
---
+++ datastructure_demo/src/main/java/com/dy/datastructure/stack/LinkedListStackDemo.java
@@ -0,0 +1,43 @@
+package com.dy.datastructure.stack;
+
+/**
+ * 链表模拟栈
+ */
+public class LinkedListStackDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+/**
+ * 链表模拟栈
+ */
+class LinkedListStack{
+
+
+
+
+
+}
+
+
+class StackNode{
+
+    public int no;
+
+    public StackNode next;
+
+    public StackNode(int no){
+        this.no=no;
+    }
+
+    @Override
+    public String toString() {
+        return "StackNode{" +
+                "no=" + no +
+                '}';
+    }
+}
